6734525a9be6893839ccc7152a7d07fe89832c7d
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 15:47:35 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-  %q{SELECT projects.title, SUM(pledges.amount)
-     FROM   projects
-     JOIN   pledges ON pledges.project_id = projects.id
+  %q{SELECT   projects.title, SUM(pledges.amount)
+     FROM     projects
+     JOIN     pledges ON pledges.project_id = projects.id
      GROUP BY projects.title;
   }
 end
