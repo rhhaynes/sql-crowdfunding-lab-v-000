f12c4f17084b9dcfe1c3188c11a8ffb58bca6ad8
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 15:55:54 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,10 +15,10 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  %q{SELECT   projects.title, SUM(projects.funding_goal)-projects.funding_goal
+  %q{SELECT   projects.title, SUM(pledges.amount)-projects.funding_goal AS 'amount_over_goal'
      FROM     projects
      JOIN     pledges ON pledges.project_id = projects.id
-     GROUP BY projects.name HAVING 
+     GROUP BY projects.name HAVING SUM(pledges.amount) >= projects.funding_goal
   }
 end
 
