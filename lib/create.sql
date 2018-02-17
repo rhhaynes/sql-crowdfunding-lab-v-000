0d0834aeb821dbd74669afe06e63dee16d05c18b
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-17 15:24:48 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,4 @@
-CREATE TABLE project (id INTEGER PRIMARY KEY,
-  title        TEXT,
-  category     TEXT,
-  funding_goal INTEGER,
-  start_date   TEXT,
-  end_date     TEXT
-);
+CREATE TABLE project (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
 
 CREATE TABLE user (
   id           INTEGER PRIMARY KEY,
